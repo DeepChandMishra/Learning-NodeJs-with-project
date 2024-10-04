4d2137fcd67f3ba10c1b737bed9fb97f328c01c2
DeepChandMishra <deepchandmishra90761@gmail.com>
2024-10-04 14:29:36 +0530

    sign up page added

---
+++ client/src/components/SignUp.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+
+const SignUp = () => {
+    const [name,setName]=useState('')
+    const [password,setPassword]=useState('');
+    const [email,setEmail]=useState('');
+    const collectData=()=>{
+        console.log(name,email,password)
+    }
+
+  return (
+    <div className="register">
+        <h1>Register</h1>
+        <input className="inputBox" type="text" value={name} onChange={(e)=>{setName(e.target.value)}} placeholder="enter name" />
+        <input className="inputBox" type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}} placeholder="enter email" />
+        <input className="inputBox" type="password" value={password} onChange={(e)=>{setPassword(e.target.value)}} placeholder="enter password" />
+        <button className="appbutton" type="button" onClick={collectData}>Sign Up</button>
+    </div>
+  )
+}
+
+export default SignUp
